bba032378275b526c2de1ae0a472fd8727562f24
Gabriel Yu <gabrielyu.22@ichat.sp.edu.sg>
2023-10-30 21:29:02 +0800

    Exercise 3

--- Lab2.py
+++ Lab2.py
@@ -1,9 +1,13 @@
 def main():
     display_main_menu()
     num_list = get_user_input()
-    calc_average_temperature(num_list)
-    calc_min_max_temperature(num_list)
-    calc_median_temperature(num_list)
+    average = calc_average_temperature(num_list)
+    print("Average=",average)
+    maximum, minimum = calc_min_max_temperature(num_list)
+    print("Max= ", maximum)
+    print("Min= ", minimum)
+    median = calc_median_temperature(num_list)
+    print("Median= ", median)
 
 def display_main_menu():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
@@ -12,25 +16,23 @@ def get_user_input():
     userInput = input("Enter some numbers separated by commas (e.g. 5, 67, 32)"+ "\n")
     list = userInput.split(",")
     float_list = [float(i) for i in list]
-    print (float_list)
     return float_list
 
 def calc_average_temperature(list):
     average = sum(list)/ len(list)
-    print("Average= ",average)
     return average
 
 def calc_min_max_temperature(list):
     maximum = max(list)
     minimum = min(list)
-    print("Max= " ,maximum)
-    print("Min= " ,minimum)
+    return maximum, minimum
 
 def calc_median_temperature(list):
     x = sorted(list)
-    print("Sorted list is " , x)
     mid = len(list) // 2
     result = (list[mid] + list[~mid]) / 2
-    print("Median= ", result)
+    return result
 
-main()
+
+if __name__ == '__main__':
+    main()
---
+++ Test_Lab2.py
@@ -0,0 +1,25 @@
+import Lab2
+
+print("Test_Lab2")
+
+def test_find_min_max():
+    input_arr = [1,2,3,4,5]
+    test_max = 5
+    test_min = 1
+    maximum,minimum = Lab2.calc_min_max_temperature(input_arr)
+
+    assert (test_max == maximum and test_min == minimum)
+
+def test_calc_average():
+    input_arr = [1,2,3,4,5]
+    test_average = 3
+    average = Lab2.calc_average_temperature(input_arr)
+
+    assert (test_average == average)
+
+def test_calc_median_temperature():
+    input_arr = [1,2,3,4,5]
+    test_median = 3
+    median = Lab2.calc_median_temperature(input_arr)
+
+    assert (test_median == median)
