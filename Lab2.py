862a5315377e036f6b73f16eac4c72f2086f5499
Gabriel Yu <gabrielyu.22@ichat.sp.edu.sg>
2023-11-26 22:40:14 +0800

    bug fix

--- Lab2.py
+++ Lab2.py
@@ -2,30 +2,35 @@ def main():
     display_main_menu()
     num_list = get_user_input()
     average = calc_average_temperature(num_list)
-    print("Average=",average)
+    print("Average=", average)
     maximum, minimum = calc_min_max_temperature(num_list)
     print("Max= ", maximum)
     print("Min= ", minimum)
     median = calc_median_temperature(num_list)
     print("Median= ", median)
 
+
 def display_main_menu():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
 
+
 def get_user_input():
-    userInput = input("Enter some numbers separated by commas (e.g. 5, 67, 32)"+ "\n")
+    userInput = input("Enter some numbers separated by commas (e.g. 5, 67, 32)" + "\n")
     list = userInput.split(",")
     float_list = [float(i) for i in list]
     return float_list
 
+
 def calc_average_temperature(list):
-    average = sum(list)/ len(list)
+    average = sum(list) / len(list)
     return average
+
 
 def calc_min_max_temperature(list):
     maximum = max(list)
     minimum = min(list)
     return maximum, minimum
+
 
 def calc_median_temperature(list):
     x = sorted(list)
--- bmi.py
+++ bmi.py
@@ -1,21 +1,22 @@
 def calculate_bmi(height, weight):
     print("Height = " + str(height) + "m")
     print("Weight = " + str(weight) + "kg")
-    bmi = weight/(height*height)
-    roundedBMI = round(bmi,2)
-    print ("BMI = " + str(bmi))
-    print ("BMI (2dp) = " + str(roundedBMI))
+    bmi = weight / (height * height)
+    roundedBMI = round(bmi, 2)
+    print("BMI = " + str(bmi))
+    print("BMI (2dp) = " + str(roundedBMI))
 
     if (bmi <= 18.5):
-        print ("Underweight")
+        print("Underweight")
         return -1
 
     if (bmi >= 18.5 and bmi <= 25.0):
-        print ("Normal Weight")
+        print("Normal Weight")
         return 0
 
     else:
-        print ("Overweight")
+        print("Overweight")
         return 1
 
-print(calculate_bmi(weight=57, height=1.43))
+
+print(calculate_bmi(weight=57, height=1.83))
